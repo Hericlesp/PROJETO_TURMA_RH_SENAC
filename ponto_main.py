a34e57130f6a4d84e537db3b2758a4da274057aa
Hericlesp <hericlespaulo05@gmail.com>
2025-05-16 12:14:47 -0300

    Update ponto_main.py

--- ponto_main.py
+++ ponto_main.py
@@ -1,4 +1,5 @@
-from tkinter import Button, Entry, Label, PhotoImage,Tk
+import tkinter as tk
+from tkinter import *
 from tkinter import ttk
 import datetime as dt
 import sqlite3
@@ -49,30 +50,50 @@ def criar_banco():
     conn.close()
     
 #modelo projeto 
-import tkinter as tk
+# import tkinter as tk
 
-janela = tk.Tk()
-janela.title("Layout com Vários Frames")
+# janela = tk.Tk()
+# janela.title("Layout com Vários Frames")
 
-# Frame do topo
-top_frame = tk.Frame(janela, bg="skyblue")
-top_frame.pack(fill="x")
+# # Frame do topo
+# top_frame = tk.Frame(janela, bg="skyblue")
+# top_frame.pack(fill="x")
 
-# Frame lateral (esquerda)
-left_frame = tk.Frame(janela, bg="lightgreen", width=100)
-left_frame.pack(side="left", fill="y")
+# # Frame lateral (esquerda)
+# left_frame = tk.Frame(janela, bg="lightgreen", width=100)
+# left_frame.pack(side="left", fill="y")
 
-# Frame principal
-main_frame = tk.Frame(janela, bg="white")
-main_frame.pack(side="left", expand=True, fill="both")
+# # Frame principal
+# main_frame = tk.Frame(janela, bg="white")
+# main_frame.pack(side="left", expand=True, fill="both")
 
-# Adicionando widgets
-tk.Label(top_frame, text="Cabeçalho", bg="skyblue").pack(pady=10)
-tk.Label(left_frame, text="Menu", bg="lightgreen").pack(pady=10)
-tk.Label(main_frame, text="Área principal", bg="white").pack(pady=20)
+# # Adicionando widgets
+# tk.Label(top_frame, text="Cabeçalho", bg="skyblue").pack(pady=10)
+# tk.Label(left_frame, text="Menu", bg="lightgreen").pack(pady=10)
+# tk.Label(main_frame, text="Área principal", bg="white").pack(pady=20)
 
-janela.mainloop()
+# janela.mainloop()
 
 #notas importantes
 # relief	Estilo da borda (flat, raised, sunken, groove, ridge)
 # bd	Espessura da borda
+
+
+root = tk.Tk()
+root.title("Registro de ponto")
+root.geometry("900x500")
+#formata ção da janela
+root.minsize(900, 500)
+root.resizable(False, True)
+root.configure(bg= "lightblue")
+root.iconbitmap(r'C:\Users\Justino\Documents\Hexa\git&github\PROJETO_TURMA_RH_SENAC\pin.png')
+
+#barra de menu
+top_frame = tk.Frame(root, bg= "#F09001", width= 900, height= 50)
+top_frame.pack(side= "top", fill="x")
+
+title_lbl = tk.Label(top_frame, text= "Ponto Eletrônico", font= ("Arial", 20), bg= "#F09001", fg= "black")
+title_lbl.pack(pady=10)
+
+
+root.mainloop()
